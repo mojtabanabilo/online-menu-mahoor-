65de12e7c09a9e4939b612cd57d89bd1c57a0a92
mojtaba nabilo <mojjtaba00@gmail.com>
2024-04-04 03:05:08 +0430

    modal responsive

--- online-menu/src/components/modal/Modal.tsx
+++ online-menu/src/components/modal/Modal.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./modal.module.scss";
 import logo from "../../assets/logo/Capture.png";
-import { TypeAnimation } from 'react-type-animation';
+import { TypeAnimation } from "react-type-animation";
 
 const Modal = (props: {
   modal: {
@@ -13,16 +13,12 @@ const Modal = (props: {
       <img src={logo} alt="mahoor" />
       <h1>! سلام</h1>
       <TypeAnimation
-      sequence={[
-        // Same substring at the start will only be typed out once, initially
-        'به سفره خانه ماهور خوش آمدید',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-      ]}
-      wrapper="h3"
-      speed={50}
-      style={{ fontSize: '1.2rem', display: 'inline-block'}}
-      repeat={1}
-    />
+        sequence={["به سفره خانه ماهور خوش آمدید", 1000]}
+        wrapper="h3"
+        speed={60}
+        style={{ fontSize: "1.2rem", display: "inline-block" }}
+        repeat={1}
+      />
       <button onClick={() => props.modal.setModal(false)}>نمایش منو</button>
     </section>
   );
